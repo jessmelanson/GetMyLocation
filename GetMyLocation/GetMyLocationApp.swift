15ffa7793ef194561ea2e2af53a0193bce17e4b9
Jess Melanson <jmelanson38@massmutual.com>
2020-12-14 09:51:12 -0500

    Initial Commit

---
+++ GetMyLocation/GetMyLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetMyLocationApp.swift
+//  GetMyLocation
+//
+//  Created by Melanson, Jessica on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct GetMyLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
